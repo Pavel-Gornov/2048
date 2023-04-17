b55e5997d89ea6d625143a14a7101d6e100304cd
Pavel-Gornov <gornovPavel30@yandex.ru>
2023-04-18 00:36:27 +0400

    0.2

--- app/src/main/java/com/example/a2048/MainMenuActivity.java
+++ app/src/main/java/com/example/a2048/MainMenuActivity.java
@@ -2,33 +2,25 @@ package com.example.a2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import java.io.IOException;
 
 public class MainMenuActivity extends AppCompatActivity {
-    protected ImageView logo;
-    public Bitmap main_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
-        logo = findViewById(R.id.menu_logo);
-        try {
-            main_menu = BitmapFactory.decodeStream(this.getAssets().open("main_menu.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        logo.setImageBitmap(main_menu);
-
     }
 
     public void btn_new_game_click(View view) {
         Intent g = new Intent(this, GameActivity.class);
         startActivity(g);
+    }
+
+    public void btn_continue_click(View view) {
+    }
+
+    public void btn_settings_click(View view) {
     }
 }
