be8c97f1ed1b7bac0bfebfd7155974313982232b
Pavel-Gornov <gornovpavel30@yandex.ru>
2023-05-22 19:10:44 +0400

    Первые успехи с firebase.

--- app/src/main/java/com/PGgames/a2048/MainMenuActivity.java
+++ app/src/main/java/com/PGgames/a2048/MainMenuActivity.java
@@ -7,13 +7,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import com.PGgames.a2048.databinding.MainMenuBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainMenuActivity extends AppCompatActivity {
+    private static final String TAG = "MainMenuActivity";
     private MainMenuBinding binding;
     protected Intent g;
     protected SharedPreferences sharedPref;
+    @SuppressLint("HardwareIds")
+    private final String ANDROID_ID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,15 @@ public class MainMenuActivity extends AppCompatActivity {
         binding = MainMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         sharedPref = getSharedPreferences(Keys.PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users");
+        myRef.get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.e(TAG, "Error getting data", task.getException());
+            } else {
+                DataSnapshot res = (DataSnapshot) task.getResult().getValue();
+                Log.d(TAG, String.valueOf(res));
+            }
+        });
     }
 
     @SuppressLint("SetTextI18n")
