e894a53f056c958eb1ce394cb7c29a616c13c767
Pavel-Gornov <gornovPavel30@yandex.ru>
2023-05-23 00:47:00 +0400

    +- рабочие облачные сохранения рекордов.

--- app/src/main/java/com/PGgames/a2048/Keys.java
+++ app/src/main/java/com/PGgames/a2048/Keys.java
@@ -7,4 +7,5 @@ public class Keys {
     public static final String SAVE_KEY = "SAVE_KEY";
     public static final String PREFERENCE_FILE_KEY = "com.PGgames.a2048.pref";
     public static final String DEFLATE_BOARD_STATE = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
+    public static final String USER_ID_KEY = "USER_ID_KEY";
 }
--- app/src/main/java/com/PGgames/a2048/MainMenuActivity.java
+++ app/src/main/java/com/PGgames/a2048/MainMenuActivity.java
@@ -6,22 +6,24 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+
 import com.PGgames.a2048.databinding.MainMenuBinding;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Scanner;
 
 public class MainMenuActivity extends AppCompatActivity {
     private static final String TAG = "MainMenuActivity";
     private MainMenuBinding binding;
     protected Intent g;
     protected SharedPreferences sharedPref;
-    @SuppressLint("HardwareIds")
-    private final String ANDROID_ID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+    protected String user_id;
+    protected int high_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +31,21 @@ public class MainMenuActivity extends AppCompatActivity {
         binding = MainMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         sharedPref = getSharedPreferences(Keys.PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        user_id = get_user_id();
+        high_score = sharedPref.getInt(Keys.HIGH_SCORE_KEY, 0);
         DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users");
-        myRef.get().addOnCompleteListener(task -> {
+        myRef.child(user_id).get().addOnCompleteListener(task -> {
             if (!task.isSuccessful()) {
                 Log.e(TAG, "Error getting data", task.getException());
             } else {
-                DataSnapshot res = (DataSnapshot) task.getResult().getValue();
+                com.google.firebase.database.DataSnapshot res = task.getResult();
+                if (res != null) {
+                    String record_string = String.valueOf(res.child("record").getValue());
+                    Scanner in = new Scanner(record_string);
+                    int record = in.nextInt();
+                    if (record > high_score)
+                        high_score = record;
+                }
                 Log.d(TAG, String.valueOf(res));
             }
         });
@@ -44,8 +55,13 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        int high_score = sharedPref.getInt(Keys.HIGH_SCORE_KEY, 0);
         binding.highScoreTxt.setText(getResources().getString(R.string.your_high_score) + " " + high_score);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        update_db();
     }
 
     public void btn_new_game_click(View view) {
@@ -62,5 +78,32 @@ public class MainMenuActivity extends AppCompatActivity {
     public void btn_settings_click(View view) {
         g = new Intent(this, SettingsActivity.class);
         startActivity(g);
+    }
+
+    public String generate_hardware_id() {
+        return "000" +
+                Build.BOARD.length() % 10 + Build.BRAND.length() % 10 +
+                Build.DEVICE.length() % 10 + Build.DISPLAY.length() % 10 +
+                Build.HOST.length() % 10 + Build.ID.length() % 10 +
+                Build.MANUFACTURER.length() % 10 + Build.MODEL.length() % 10 +
+                Build.PRODUCT.length() % 10 + Build.TAGS.length() % 10 +
+                Build.TYPE.length() % 10 + Build.USER.length() % 10;
+    }
+
+    public String get_user_id() {
+        String id = sharedPref.getString(Keys.USER_ID_KEY, null);
+        if (id == null) {
+            id = generate_hardware_id();
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(Keys.USER_ID_KEY, id);
+            editor.apply();
+        }
+        return id;
+    }
+
+    public void update_db() {
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users");
+        myRef.child(user_id).child("record").setValue(high_score);
+        myRef.push();
     }
 }
